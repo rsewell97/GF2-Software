1ada033be72260eba3b9dd52ef45cd272b36e9ab
Lea GP <leagp@btinternet.com>
2019-05-26 11:52:22 +0100

    this might be a big mistake....

--- main_project/test_names.py
+++ main_project/test_names.py
@@ -1,6 +1,5 @@
 """Test the names module."""
 import pytest
-
 from names import Names
 
 
@@ -13,7 +12,7 @@ def new_names():
 @pytest.fixture
 def name_string_list():
     """Return a list of example names."""
-    return ["Alice", "Bob", "Eve"]
+    return ["Lea", "Robbie", "Dan"]
 
 
 @pytest.fixture
@@ -34,3 +33,42 @@ def test_get_name_string_raises_exceptions(used_names):
     with pytest.raises(ValueError):
         used_names.get_name_string(-1)
 
+
+@pytest.mark.parametrize("tag, id", [("Lea",7), ("Robbie",2), ("Dan", 8)])
+def test_expected_outputs(new_names, used_names, tag, id):
+    print(used_names(tag))
+    #print(new_names.get_name_string(id))
+    #print(used_names.get_name_string(id))
+    assert 0
+
+# def test_lookup_raises_exceptions(name_string_list):
+#     """ Test if lookup raises expected exceptions."""
+#     with pytest.raises(TypeError):
+#         name_string_list.lookup(12)
+#     with pytest.raises(TypeError):
+#         name
+
+
+def test_combination_funcs(name_string_list,used_names):
+    '''Check that the query function on a single string works the same as the lookup function on an index list'''
+    i = 0
+    for name in name_string_list:
+        assert used_names.lookup(name)[i] == used_names.query(name[i])
+        assert name_string_list[i] == used_names.get_name_string(used_names.lookup(name_string_list)[i])
+        i +=1
+
+
+def test_types(used_names,name_string_list):
+    i = 0
+    while i <= 2:
+        assert (type(used_names.get_name_string(used_names.lookup(name_string_list)[i]))) is str
+        assert (type(used_names.lookup(name_string_list)[i])) is int
+        assert (type(used_names.query(name_string_list[i]))) is int
+        i += 1
+
+# def test_query(used_names, name_string_list):
+#     #used_names.lookup(name_string_list)
+#     (used_names.query("Lea"))
+#     print(used_names.query("Robbie"))
+#     print(used_names.query("Dan"))
+#     assert 0
