672ea562bb77905e3b7ab8ce5aa53cd6e6a5ac2c
Lea GP <leagp@btinternet.com>
2019-05-26 13:15:00 +0100

    pytests for names.py

--- main_project/test_names.py
+++ main_project/test_names.py
@@ -34,12 +34,18 @@ def test_get_name_string_raises_exceptions(used_names):
         used_names.get_name_string(-1)
 
 
-@pytest.mark.parametrize("tag, id", [("Lea",7), ("Robbie",2), ("Dan", 8)])
-def test_expected_outputs(new_names, used_names, tag, id):
-    print(used_names(tag))
-    #print(new_names.get_name_string(id))
-    #print(used_names.get_name_string(id))
-    assert 0
+@pytest.mark.parametrize("tag, id", [("Lea",9), ("Robbie",10), ("Dan", 11)])
+def test_get_name(new_names, name_string_list, used_names, tag, id):
+    assert used_names.get_name_string(id) == tag
+    assert new_names.get_name_string(id) is None
+
+
+@pytest.mark.parametrize("tag, id", [("Lea",9), ("Robbie",10), ("Dan", 11)])
+def test_query(new_names, name_string_list, used_names, tag, id):
+    print(used_names.lookup(tag))
+    assert used_names.query(tag) == id
+    assert new_names.query(tag) is None
+
 
 # def test_lookup_raises_exceptions(name_string_list):
 #     """ Test if lookup raises expected exceptions."""
@@ -53,6 +59,8 @@ def test_combination_funcs(name_string_list,used_names):
     '''Check that the query function on a single string works the same as the lookup function on an index list'''
     i = 0
     for name in name_string_list:
+        print (used_names.lookup(name)[i])
+        print(used_names.get_name_string(used_names.lookup(name_string_list)[i]))
         assert used_names.lookup(name)[i] == used_names.query(name[i])
         assert name_string_list[i] == used_names.get_name_string(used_names.lookup(name_string_list)[i])
         i +=1
