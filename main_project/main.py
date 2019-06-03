eba912ff53e40dbec4e22b222413feb84637f7e7
Robbie <rsewell97@googlemail.com>
2019-06-03 10:31:19 +0100

    loacalisation test

--- main_project/main.py
+++ main_project/main.py
@@ -11,7 +11,7 @@ Command line user interface: logsim.py -c <file path>
 Graphical user interface: logsim.py <file path>
 """
 import getopt
-import sys
+import sys, os
 
 import wx
 
@@ -28,16 +28,100 @@ from error import Error
 #_________________IMPORTANT: CHANGE GUI MODULE________________#
 
 
-app = wx.App()
+# language domain
+langDomain = "LOGIC SIM APP"
+# languages you want to support
+supLang = {u"en": wx.LANGUAGE_ENGLISH,
+           u"fr": wx.LANGUAGE_FRENCH,
+           u"de": wx.LANGUAGE_GERMAN,
+          }
+
+
+def _displayHook(obj):
+    if obj is not None:
+        print (repr(obj))
+
+# add translation macro to builtin similar to what gettext does
+import builtins
+builtins.__dict__['_'] = wx.GetTranslation
+
+
+from wx.lib.mixins.inspection import InspectionMixin
+
+class BaseApp(wx.App, InspectionMixin):
+    def OnInit(self):
+        self.Init() # InspectionMixin
+        # work around for Python stealing "_"
+        sys.displayhook = _displayHook
+        
+        self.appName = "Logic Simulator"
+        
+        self.doConfig()
+        
+        self.locale = None
+        wx.Locale.AddCatalogLookupPathPrefix('locale')
+        self.updateLanguage(self.appConfig.Read(u"Language"))
+
+        return True
+
+    def doConfig(self):
+        """Setup an application configuration file"""
+        # configuration folder
+        sp = wx.StandardPaths.Get()
+        self.configLoc = sp.GetUserConfigDir()
+        self.configLoc = os.path.join(self.configLoc, self.appName)
+        # win: C:\Users\userid\AppData\Roaming\appName
+        # nix: \home\userid\appName
+
+        if not os.path.exists(self.configLoc):
+            os.mkdir(self.configLoc)
+
+        # AppConfig stuff is here
+        self.appConfig = wx.FileConfig(appName=self.appName,
+                                       vendorName=u'Robbie Sewell, Lea Ganser-Potts, Daniel MacKinnon',
+                                       localFilename=os.path.join(
+                                       self.configLoc, "AppConfig"))
+    
+        if not self.appConfig.HasEntry(u'Language'):
+            # on first run we default to German
+            self.appConfig.Write(key=u'Language', value=u'de')
+            
+        self.appConfig.Flush()
+
+    def updateLanguage(self, lang):
+        """
+        Update the language to the requested one.
+        
+        Make *sure* any existing locale is deleted before the new
+        one is created.  The old C++ object needs to be deleted
+        before the new one is created, and if we just assign a new
+        instance to the old Python variable, the old C++ locale will
+        not be destroyed soon enough, likely causing a crash.
+        
+        :param string `lang`: one of the supported language codes
+        
+        """
+        # if an unsupported language is requested default to English
+        if lang in supLang:
+            selLang = supLang[lang]
+        else:
+            selLang = wx.LANGUAGE_ENGLISH
+            
+        if self.locale:
+            assert sys.getrefcount(self.locale) <= 2
+            del self.locale
+        
+        # create a locale object for this language
+        self.locale = wx.Locale(selLang)
+        if self.locale.IsOk():
+            self.locale.AddCatalog(langDomain)
+        else:
+            self.locale = None
+
+
+
+app = BaseApp()
 gui = Gui("Logic Simulator")
 gui.Show(True)
 app.MainLoop()
-
-# language domain
-#langDomain = "I18Nwxapp"
-# languages you want to support
-#supLang = {u"en": wx.LANGUAGE_ENGLISH,
- #          u"fr": wx.LANGUAGE_FRENCH,
- #         }
-
 
